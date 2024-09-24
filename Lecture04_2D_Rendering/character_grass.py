baf487920a763f88d1c78208827d1b59768662e1
hyeondoong2 <sallybi@naver.com>
2024-09-24 11:49:48 +0900

    뼈대 완성 print로 무한루프확인

--- Lecture04_2D_Rendering/character_grass.py
+++ Lecture04_2D_Rendering/character_grass.py
@@ -2,6 +2,24 @@ from pico2d import *
 
 open_canvas()
 
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
+def run_circle():
+    print('circle')
+    
+    pass
+
+def run_rectangle():
+    print('rectangle')
+    
+    pass
+    
+while True:
+    run_circle()
+    run_rectangle()
+
+    
 # fill here
 
 close_canvas()
