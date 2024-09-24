28dc0e578850df6254ccbd01ed3bb423767bea0a
hyeondoong2 <sallybi@naver.com>
2024-09-24 12:38:12 +0900

    오른쪽 이동 완성

--- Lecture04_2D_Rendering/character_grass.py
+++ Lecture04_2D_Rendering/character_grass.py
@@ -35,6 +35,8 @@ def run_top():
 
 def run_right():
     print('right')
+    for y in range(550, 0, -10):
+        draw_boy(790, y)
     pass
 
 def run_bottom():
@@ -50,7 +52,7 @@ def run_left():
 
 def run_rectangle():
     print('rectangle')
-    run_top()
+    #run_top()
     run_right()
     run_bottom()
     run_left()
