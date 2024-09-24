a8cb26c607edad08958008341443d28f36abca84
hyeondoong2 <sallybi@naver.com>
2024-09-24 12:12:16 +0900

    원 운동 구현 완료

--- Lecture04_2D_Rendering/character_grass.py
+++ Lecture04_2D_Rendering/character_grass.py
@@ -1,4 +1,6 @@
 from pico2d import *
+import math
+
 
 open_canvas()
 
@@ -8,9 +10,15 @@ boy = load_image('character.png')
 def run_circle():
     print('circle')
 
-    clear_canvas_now()
-    boy.draw_now(400,300)
-    delay(0.1)
+    r, cx, cy = 300, 800//2, 600/2
+    for deg in range(0, 360, 3):
+        theta = math.radians(deg)
+        x =  r * math.cos(theta) + cx
+        y = r * math.sin(theta) + cy
+        
+        clear_canvas_now()
+        boy.draw_now(x, y)
+        delay(0.1)
     
     pass
 
